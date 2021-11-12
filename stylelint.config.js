05e22224d2d52cf72734391fdd5243300fb6f0b4
Val Novikov <ipamoon@gmail.com>
2021-11-12 22:41:05 +0400

    fix(eslint): update config to ignore linting docs folder

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-prettier'
   ],
+  ignoreFiles: ['docs/**/*'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {}
